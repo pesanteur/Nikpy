41471a72464a7b8c62bae3d67149eb72eb113d0f
Mathue <pesanteur123@gmail.com>
2017-04-23 17:17:21 -0400

    Username and password is now pulled from the environment. In future allow user to select as well.

--- __main__.py
+++ __main__.py
@@ -1,17 +1,39 @@
 from selenium import webdriver
+# this module is used to pull information from environ
+# example: environ.get('USERNAME')
+# exported as export USERNAME='<insert username here>'
+from os import environ
 
 url = "http://www.nikkyo.gr.jp"
 
 browser = webdriver.Chrome()
 browser.get(url)
 
-def login(username, password):
+def login(user, pwd):
 	username = browser.find_element_by_id('userlogin')
-	password = browser.findd_element_by_id('password')
-	username.send_keys(username)
-	password.send_keys(password)
+	password = browser.find_element_by_id('password')
+	username.send_keys(user)
+	password.send_keys(pwd)
 	password.submit()
-	
+
 def get_stock_page():
-	stock_elem = browser.find_element_by_link_text('STOCK')
+	stock_elem = browser.find_element_by_link_text('MY NIKKYO')
 	stock_elem.click()
+
+def get_acct_page():
+	acct_elem = browser.find_element_by_id('account_report_btn')
+	acct_elem.click()
+
+def input_start_date():
+	search_date = browser.find_element_by_id('srch_term_start')
+	search_date.send_keys('2016/06/14')
+	search_btn = browser.find_element_by_id('btn_srch')
+	search_btn.click()
+
+if __name__ == "__main__":
+	user = environ.get('USERNAME')
+	pwd = environ.get('PASSWORD')
+	login(user, pwd)
+	get_stock_page()
+	get_acct_page()
+	input_start_date()
