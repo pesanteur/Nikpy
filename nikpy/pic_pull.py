f5c5861beedf53ae4f9c56be46bb2b81ef9cc2a8
Pesanteur <pesanteur123@gmail.com>
2017-05-20 14:45:00 -0400

    Working on pulling tables from older documents.

--- nikpy/pic_pull.py
+++ nikpy/pic_pull.py
@@ -21,28 +21,36 @@ def pic_pull(browser): # maybe change this to just get pics and then create anot
                 continue
             else:
                 print('Table not uploaded') #TODO: Start pulling tables here. How to make pull happen with viewing below...maybe remove other else statement?
+                car_links.append((code_no,pic_url))
+                # This breaks main code somehow TODO: Fix this later
+                for code, link in car_links:
+                    browser.get(link)
+                    car_info_table = get_table_data(browser)[0] # gets html table data about the cars || This works after testing with pdb
+
+                    table_links.append((code, car_info_table))
             continue
         else:
             car_links.append((code_no,pic_url))
 
     pic_links = []
 
-    for code, link in car_links:
-        print('Navigating to car with CODE no.: %s' % code)
-        browser.get(link)
+    if not table_links:
+        for code, link in car_links:
+            print('Navigating to car with CODE no.: %s' % code)
+            browser.get(link)
 
-        car_info_table = get_table_data(browser)[0] # gets html table data about the cars || This works after testing with pdb
-        thumbnails = browser.find_elements_by_class_name('poppic')
+            car_info_table = get_table_data(browser)[0] # gets html table data about the cars || This works after testing with pdb
+            thumbnails = browser.find_elements_by_class_name('poppic')
 
-        inner_links = []
-        print('Downloading URLs from thumbnails for CODE no.: %s' % code)
-        for picture in thumbnails:
-            car_pic = picture.get_attribute('href')
-            basename = os.path.basename(car_pic)
-            print("URL for pic %s grabbed" % basename)
-            inner_links.append(car_pic)
-       #pic_links.append(((code, car_info_table), inner_links)) # pic_links is structured like this so it can be turned into a dictionary
-        table_links.append((code, car_info_table)) # This works as well therefore issue must be in nikpy.py
-        pic_links.append((code, inner_links)) # pic_links is structured like this so it can be turned into a dictionary
+            inner_links = []
+            print('Downloading URLs from thumbnails for CODE no.: %s' % code)
+            for picture in thumbnails:
+                car_pic = picture.get_attribute('href')
+                basename = os.path.basename(car_pic)
+                print("URL for pic %s grabbed" % basename)
+                inner_links.append(car_pic)
+           #pic_links.append(((code, car_info_table), inner_links)) # pic_links is structured like this so it can be turned into a dictionary
+            table_links.append((code, car_info_table)) # This works as well therefore issue must be in nikpy.py
+            pic_links.append((code, inner_links)) # pic_links is structured like this so it can be turned into a dictionary
 
     return pic_links, table_links # switch from car_links to pic_links
